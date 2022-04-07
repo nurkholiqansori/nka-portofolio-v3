60d1f26325017a0079cfe68530d029e515a71b1c
Nur Kholiq Ansori <nurkholiqansori8@gmail.com>
2022-04-07 11:09:56 +0700

    Common fixed

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,24 +1,22 @@
-import { useColorMode, Switch, FormControl, FormLabel } from '@chakra-ui/react'
+import { useColorMode, Switch, FormControl } from '@chakra-ui/react'
 
 export const DarkModeSwitch = () => {
   const { colorMode, toggleColorMode } = useColorMode()
   const isDark = colorMode === 'dark'
   return (
     <>
-      <FormControl>
-        <Switch
-          id='dark-mode'
-          position='fixed'
-          top='1rem'
-          right='1rem'
-          color='green'
-          isChecked={isDark}
-          onChange={toggleColorMode}
-        />
-        <FormLabel htmlFor='dark-mode' hidden>
-          Dark Mode
-        </FormLabel>
-      </FormControl>
+      <Switch
+        id='dark-mode'
+        position='fixed'
+        top='1rem'
+        right='1rem'
+        color='green'
+        isChecked={isDark}
+        onChange={toggleColorMode}
+      />
+      <label htmlFor='dark-mode' hidden>
+        Dark Mode
+      </label>
     </>
   )
 }
