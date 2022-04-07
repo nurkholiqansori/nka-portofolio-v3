f01d038ae7b9ec251a121e04a26055203bd007b0
Nur Kholiq Ansori <nurkholiqansori8@gmail.com>
2022-04-07 11:29:45 +0700

    Styling dark mode button

--- src/components/DarkModeSwitch.tsx
+++ src/components/DarkModeSwitch.tsx
@@ -1,22 +1,33 @@
-import { useColorMode, Switch, FormControl } from '@chakra-ui/react'
+import { useColorMode, Switch, Button, FormLabel, Collapse } from '@chakra-ui/react'
+import React from 'react'
 
 export const DarkModeSwitch = () => {
+  const [ show, setShow ] = React.useState(false)
   const { colorMode, toggleColorMode } = useColorMode()
   const isDark = colorMode === 'dark'
   return (
     <>
-      <Switch
-        id='dark-mode'
+      <Button
         position='fixed'
         top='1rem'
         right='1rem'
-        color='green'
-        isChecked={isDark}
-        onChange={toggleColorMode}
-      />
-      <label htmlFor='dark-mode' hidden>
-        Dark Mode
-      </label>
+        onMouseEnter={() => setShow(true)}
+        onMouseLeave={() => setShow(false)}
+        leftIcon={
+          <Switch
+            id='dark-mode'
+            color='green'
+            isChecked={isDark}
+            onChange={toggleColorMode}
+          />
+        }
+      >
+        <Collapse in={show}>
+        <FormLabel htmlFor='dark-mode' pt='1.5' cursor='pointer'>
+          Dark Mode
+        </FormLabel>
+        </Collapse>
+      </Button>
     </>
   )
 }
